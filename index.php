7f20cca0ca3bb4586bb33348425c639fa1a9444a
Go3shom <shadi.muhammad001@gmail.com>
2020-11-07 16:03:22 +0200

    init

---
+++ index.php
@@ -0,0 +1,296 @@
+<?php
+/*
+    Theme Name: Go3shom
+    Unique Name: Go3shom
+    Theme URI: https://harisl.net/
+    Author: ThemeWnaTheme Author
+    Author URI: https://go3shom.com
+    Version: 1.0.0
+    Description: Custom theme description
+*/
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/custom.css">
+
+    <title>
+        Harisl
+    </title>
+</head>
+
+<body>
+    <!-- START NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="" aria-label="harisl">
+            <img class="img-fluid logo" src="./assets/images/harbot-logo.png" alt="harbot-logo">
+            <!-- <a class="nav-link d-none" href=""> -->
+            <!-- Home -->
+            <span class="sr-only">(current)</span>
+            <!-- </a> -->
+        </a>
+
+        <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./login.html">Why a Bot</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#work">Work</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#faq">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+            <span class="bg-danger p-2">
+                <small class="text-warning d-block py-3 mx-0">Call Us Now</small>
+                <small class="text-light ">+12345678</small>
+            </span>
+        </div>
+        <!-- ./navbar-collapse -->
+
+    </nav>
+    <!-- END NAVBAR -->
+
+
+
+
+    <div class="container-fluid">
+        <div class="row bg-image bg-light">
+            <div class="col-sm-7 col-md-5 py-3 my-3">
+                <h1 class="h1 display-4 py-md-5">
+                    <!-- <h1 class="h1"> -->
+                    Turn Conversation Into
+                    <span class="text-danger">a Better </span>
+                    Engagement
+                </h1>
+                <button class="btn btn-danger btn-lg rounded-pill mt-3">Customize Your Bot
+                    <strong>Today!</strong>
+                </button>
+
+            </div>
+            <!-- ./col-7 -->
+        </div>
+        <!-- ./row -->
+    </div>
+    <!-- ./container-fluid -->
+
+
+    <div class="container">
+        <div class="my-5">
+
+            <div class="row text-center">
+                <div class="col-sm-6 col-md-3 pb-4">
+                    <div class="d-lg-flex flex-md-row">
+                        <i aria-hidden="true" class="pr-2 fa fa-globe fa-5x fa-primary"></i>
+                        <div>
+                            <strong class="h4">3 Billion</strong>
+                            <p class="">Users</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+
+                <div class="col-sm-6 col-md-3 pb-4">
+                    <div class="d-lg-flex flex-md-row">
+                        <i class="d-sm-inline-flex pr-2 fa fa-shopping-cart fa-5x fa-primary" aria-hidden="true"></i>
+                        <div class="">
+                            <strong class="h2">30%</strong>
+                            <p>Conversation</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+
+                <div class="col-sm-6 col-md-3 pb-4">
+                    <div class="d-lg-flex flex-md-row">
+                        <i class="d-sm-inline-flex pr-2 fa fa-volume-control-phone fa-5x fa-primary" aria-hidden="true"></i>
+                        <div class="">
+                            <strong class="h2">24/7</strong>
+                            <p>Replies</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+
+                <div class="col-sm-6 col-md-3 pb-4">
+                    <div class="d-lg-flex flex-md-row">
+                        <i class="d-sm-inline-flex pr-2 fa fa-tachometer fa-5x fa-primary" aria-hidden="true"></i>
+                        <div class="">
+                            <strong class="h2">Instant</strong>
+                            <p>Answers</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./col -->
+
+            </div>
+
+        </div>
+        <!-- ./row -->
+
+    </div>
+    <!-- ./container -->
+
+
+    <div class="our-works">
+        <div class="container">
+            <!-- <h2 class="h1 text-center">Our Work</h2> -->
+            <h2 class="h1 text-center text-uppercase display-4 mb-5">Our Work</h2>
+
+            <div class="row">
+
+                <div class="col-sm-12 col-md-5">
+                    <!-- <div class="card bg-light mb-3" style="max-width: 18rem;"> -->
+                    <div class="card bg-light mb-3 w-75 mx-auto">
+                        <!-- <div class="card-header text-muted">Header</div> -->
+                        <div class="card-body font-weight-bold">
+                            <h6 class="card-subtitle mb-2 text-muted">Client 1</h6>
+                            <h5 class="card-title text-danger font-weight-bold h4">ACME Company</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <!-- ./card -->
+
+                    <div class="card bg-light mb-3 w-75 mx-auto">
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted">Client 2</h6>
+                            <h5 class="card-title text-muted font-weight-bold h4">ABC ltd</h5>
+                            <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <!-- ./card -->
+
+                    <div class="card bg-light mb-3 w-75 mx-auto">
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted">Client 3</h6>
+                            <h5 class="card-title text-muted font-weight-bold h4">Samporena tbk</h5>
+                            <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <!-- ./card -->
+
+                    <div class="card bg-light mb-3 w-75 mx-auto">
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted">Client 4</h6>
+                            <h5 class="card-title text-muted font-weight-bold h4">Altec AG</h5>
+                            <p class="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                    <!-- ./card -->
+
+                </div>
+                <!-- ./col -->
+
+                <div class="col-sm-12 col-md-7 my-5 my-md-0">
+
+                    <div class="embed-responsive embed-responsive-16by9 border-top-right">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen>
+                        </iframe>
+                    </div>
+                    <!-- ./embed-responsive -->
+
+
+                    <div class="card my-3 my-md-4 border-bottom-right">
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bolder">Home Bot Application</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Integrated movie, play on click</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="" class="btn btn-link d-flex justify-content-end">Learn more</a>
+                        </div>
+                    </div>
+                    <!-- ./card -->
+                </div>
+                <!-- ./col -->
+            </div>
+            <!-- ./row -->
+        </div>
+        <!-- ./container -->
+    </div>
+    <!-- ./our-work -->
+
+
+    <div class="our-clients">
+        <div class="container">
+
+            <h2 class="h1 text-center text-uppercase display-4 my-5">Our Clients</h2>
+
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+            </div>
+            <!-- ./row -->
+
+            <div class="row mt-4">
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6 col-lg-3 ">
+                    <img src="https://source.unsplash.com/random/500x500" alt="img-placeholder" class="img-fluid">
+                </div>
+            </div>
+            <!-- ./row -->
+
+        </div>
+        <!-- ./container -->
+    </div>
+    <!-- ./our-clients -->
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="./js/jquery.min.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="./js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
